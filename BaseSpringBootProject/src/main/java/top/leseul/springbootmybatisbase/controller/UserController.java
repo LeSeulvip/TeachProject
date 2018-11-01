2ed61d67affa0e7bac3a14b03d4e05aa623d255c
X_TryAgain <2637551443@>
2018-11-01 14:37:36 +0800

    项目初始化,图片校验码,文件上传

---
+++ BaseSpringBootProject/src/main/java/top/leseul/springbootmybatisbase/controller/UserController.java
@@ -0,0 +1,42 @@
+package top.leseul.springbootmybatisbase.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import top.leseul.springbootmybatisbase.model.UserModel;
+import top.leseul.springbootmybatisbase.service.UserService;
+import top.leseul.springbootmybatisbase.utils.JsonMessage;
+
+/**
+ * -用户信息控制器
+ * 
+ * @author leseul
+ *
+ */
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+  @Autowired
+  private UserService userService;
+
+  @RequestMapping("/login")
+  public JsonMessage login(UserModel model) throws Exception {
+    // /user/login {"user.username":"","user.password":"test-pwd"}
+    return userService.login(model);
+  }
+
+  @RequestMapping("/logout")
+  public JsonMessage logout(UserModel model) throws Exception {
+    // /user/logout {}
+    return userService.logout(model);
+  }
+
+  @RequestMapping("/getUserInfo")
+  public JsonMessage getUserInfo(UserModel model) throws Exception {
+    //  /user/getUserInfo {}
+    return userService.getUserInfo(model);
+  }
+}
