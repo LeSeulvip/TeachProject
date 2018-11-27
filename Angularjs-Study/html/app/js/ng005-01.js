4ab381f0485e6b570eaa3d6b07769e5bf2a6e86a
LeSeul <2637551441@qqcom>
2018-11-27 16:27:26 +0800

    checkbox选择框 阴影

--- Angularjs-Study/html/app/js/ng005-01.js
+++ Angularjs-Study/html/app/js/ng005-01.js
@@ -1,10 +1,10 @@
 //业务逻辑
 angular.element(document).ready(function() {
   var ctrls = angular.module('controllers');
-  ctrls.controller('MyCtrl01', ['$rootScope', '$scope', '$log', MyCtrl01]);
+  ctrls.controller('MyCtrl01', ['$scope', '$log', MyCtrl01]);
 
-  function MyCtrl01($rootScope, $scope, $log) {
-    $log.debug('in MyCtrl init.....');
+  function MyCtrl01($scope, $log) {
+    $log.debug('in MyCtrl01 init.....');
 
     //处理scope销毁
     $scope.$on('$destroy', function() {
@@ -13,5 +13,43 @@ angular.element(document).ready(function() {
 
     //业务逻辑
     $scope.welcome = '全选反选演示';
+
+    //多选数据
+    $scope.datas = [
+      { id: 100, info: '电影', title: '就是电影', selected: false },
+      { id: 101, info: '音乐', title: '哈哈', selected: false },
+      { id: 102, info: '游戏', title: '1233', selected: false },
+      { id: 103, info: '篮球', title: '额呵呵', selected: false }
+    ];
+
+    //计算属性，显示选中的id列表
+    $scope.selectedIds = function() {
+      var ids = [];
+      for (var i = 0; i < $scope.datas.length; i++) {
+        var d = $scope.datas[i];
+        if (d.selected) {
+          ids.push(d.id);
+        }
+      }
+      return ids.join(',');
+    };
+
+    //全选
+    $scope.checkAll = false;
+
+    $scope.doCheckAll = function() {
+      for (var i = 0; i < $scope.datas.length; i++) {
+        var d = $scope.datas[i];
+        d.selected = $scope.checkAll;
+      }
+    };
+
+    //反选
+    $scope.doCheckRev = function() {
+      for (var i = 0; i < $scope.datas.length; i++) {
+        var d = $scope.datas[i];
+        d.selected = !d.selected;
+      }
+    };
   }
 });
